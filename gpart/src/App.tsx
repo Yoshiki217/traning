3990ea486a1603d2e746c14b6eea6a6c7056fac2
Yoshiki217 <effort0217@icloud.com>
2022-10-03 16:31:06 +0900

    add/file

--- gpart/src/App.tsx
+++ gpart/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <h1 className='relative w-10 m-4 shadow-sm fonrt-smibold text-red'>Gpart</h1>
       <button className="bg-indigo-700 font-semibold text-white py-2 px-4 rounded">aa</button>
-      Gpart
     </div>
   );
 }
