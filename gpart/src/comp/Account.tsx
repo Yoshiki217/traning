577c099f7ef8c5198eff83adae7e3c7c33c226e4
Danh <caplovsco@gmail.com>
2022-12-09 13:45:41 +0900

    fix/ Account.tsx

--- gpart/src/comp/Account.tsx
+++ gpart/src/comp/Account.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, useContext, useEffect, useState } from "react";
+import { createContext, FC, useContext, useEffect, useRef, useState } from "react";
 import { postg } from "../api/postg";
 import { getStorage } from "../api/storage";
 import { RefreshContext } from "./Slash";
@@ -46,13 +46,14 @@ export const AccountContext = createContext<accountInfo>({
 })
 
 export const Account : FC = () => {
+    const [state, setState] = useState(true)
     const context = useContext(AccountContext)
     const refresh = useContext(RefreshContext)
     const auth = useAuth()
     const [value, setValue] = useState(context)
     const [loaded, setLoaded] = useState(false)
     const gate = useNavigate()
-    // const location = useLocation()
+    const location = useLocation()
     useEffect(()=>{
         postg('account', getStorage())
         .then((json: account)=>{
@@ -61,13 +62,20 @@ export const Account : FC = () => {
             setValue(json.accountInfo)
             setLoaded(true)
             if(!json.status){
-                // console.log('go')
-                gate('update')
-                return
+                setState(false)
+                gate("update")
+            } else {
+                setState(true)
+                gate(".")
             }
         })
         // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     }, [refresh.state])
+    useEffect(()=>{
+        if(!state){
+            gate("update")
+        }
+    }, [location.pathname])
     return (
         <>
             {
