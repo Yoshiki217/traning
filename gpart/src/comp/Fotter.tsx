da2c81b3f6aacdea60e5f31bca996584c86e5afe
Yoshiki217 <effort0217@icloud.com>
2023-01-13 14:38:28 +0900

    hotifx/header and footer

--- gpart/src/comp/Fotter.tsx
+++ gpart/src/comp/Fotter.tsx
@@ -86,7 +86,7 @@ const Fotter = () => {
             </div>
             <div className="bg-gray-100">
             <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
+                <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Gpart —
                 <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
