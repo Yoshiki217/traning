58b7b688a53ed918d6c727fcbe158d6a07c39e30
Yoshiki217 <effort0217@icloud.com>
2023-01-19 16:16:03 +0900

    fix/carender font

--- gpart/src/comp/CreateCourse.tsx
+++ gpart/src/comp/CreateCourse.tsx
@@ -69,21 +69,13 @@ export const CreateCourse : FC = () => {
                 }
                 <input type="submit" value="作成" />
             </form> */}
-            <div className="px-10 py-4">
-                <div className="h-10 w-10">
-                    <div></div>
                         <form onSubmit={onSubmit}>
-                            <section className="py-5 text-blue-50">
-                                <div>
-                                <body className="bg-gray-200">
-                                <div className="container mx-auto mt-5">
+                                <div className="container mx-auto mt-7">
                                 <div className="wrapper bg-white rounded shadow w-full ">
                                     <div className="header flex justify-between border-b p-2">
-                                    <span className="text-lg font-bold">
-                                        2020 July
-                                    </span>
                                     <div className="buttons">
-                                        <button className="p-1">
+                                        {/*  カレンダー右上にこのボタンを配置,画像をプラスボタンに変更
+                                            <button className="p-1">
                                             <svg width="1em" fill="gray" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-left-circle"  xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                             <path fill-rule="evenodd" d="M8.354 11.354a.5.5 0 0 0 0-.708L5.707 8l2.647-2.646a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708 0z"/>
@@ -96,7 +88,7 @@ export const CreateCourse : FC = () => {
                                             <path fill-rule="evenodd" d="M7.646 11.354a.5.5 0 0 1 0-.708L10.293 8 7.646 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0z"/>
                                             <path fill-rule="evenodd" d="M4.5 8a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5z"/>
                                             </svg>
-                                        </button>
+                                        </button> */}
                                     </div>
                                     </div>
                                     <table className="w-full">
@@ -460,12 +452,7 @@ export const CreateCourse : FC = () => {
                                     </table>
                                 </div>
                                 </div>
-                                </body>
-                                </div>
-                            </section>
                         </form>
-                    </div>
-                </div>
         </>
     )
 }
