19caba838244e680a78cf79abc1e4de9b5bd06fe
Yoshiki217 <effort0217@icloud.com>
2023-01-23 20:51:55 +0900

    add/SettingPage , del/"account" word

--- gpart/src/comp/Account.tsx
+++ gpart/src/comp/Account.tsx
@@ -204,8 +204,6 @@ export const Account : FC = () => {
                 <main className="relative flex-1 overflow-y-auto focus:outline-none">
                     <div className="py-6">
                         <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
-                            <h1 className="text-lg text-neutral-600">アカウント
-                            </h1>
                         </div>
                         <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
                             <Outlet/>
